10182a2541ad81b423bfbee3a70bb0b29942657e
Edga380 <a.edgaras44@gmail.com>
2024-02-21 21:24:03 +0000

    New projects

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# portfoliov2
+# Hi there! 👋 I'm Edgaras.
+
+This is my porfolio website that showcase my skills and projects I have done or worked on with other people.
+
+<img src="https://github.com/Edga380/edga380/blob/main/assets/codeguybg.png" alt="Guy sitting in front of the computer" width="100%"/>
--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,125 +1,186 @@
 // Smooth scrolling to target
-document.body.addEventListener('click', function (e) {
-    if ((e.target.tagName === 'A') && e.target.getAttribute('href') && e.target.getAttribute('href').startsWith('#')) {
-        e.preventDefault();
-        const targetId = e.target.getAttribute('href');
-        const targetElement = document.querySelector(targetId);
-        if (targetElement) {
-            const targetPosition = targetElement.offsetTop;
-            window.scroll({
-                top: targetPosition,
-                behavior: 'smooth'
-            });
-        }
+document.body.addEventListener("click", function (e) {
+  if (
+    e.target.tagName === "A" &&
+    e.target.getAttribute("href") &&
+    e.target.getAttribute("href").startsWith("#")
+  ) {
+    e.preventDefault();
+    const targetId = e.target.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    if (targetElement) {
+      const targetPosition = targetElement.offsetTop;
+      window.scroll({
+        top: targetPosition,
+        behavior: "smooth",
+      });
     }
+  }
 });
 
 //
-function ProjectIndex(index){
-    localStorage.setItem("index", index);
-    window.location.href = "./project.html"
-};
+function ProjectIndex(index) {
+  localStorage.setItem("index", index);
+  window.location.href = "./project.html";
+}
 
 // Next previous project
-function NextPreviousProject(num){
-    localStorage.setItem("index", parseInt(localStorage.getItem("index")) + num);
-    window.location.href = "./project.html"
+function NextPreviousProject(num) {
+  localStorage.setItem("index", parseInt(localStorage.getItem("index")) + num);
+  window.location.href = "./project.html";
 }
 
 // Project page populate with information from json file
 let projectIndex = localStorage.getItem("index");
 let projectImages;
 // Check if html element exist
-if(document.getElementById('slideshow-container')){
-    fetch('./projectsinfo.json')
-    .then(response => response.json())
-    .then(data => {
-        // SlideShow images
-        data[projectIndex].slideShowImg.forEach(image => {
-            document.getElementById('slideshow-container').insertAdjacentHTML('beforeend', `
-            <img class="my-slide-image ${(image === 0 ? 'active' : '')}" src="${image}">`);
-        })
-        // 
-        projectImages = document.querySelectorAll('.my-slide-image');
-        // Insert project name into destination link
-        document.getElementById("project-destination").innerText = data[projectIndex].name
-        // Insert project name
-        document.getElementById("project-name").innerText = data[projectIndex].name;
-        // Insert project description
-        document.getElementById("project-description").innerText = data[projectIndex].description.join('');
-        // Insert links
-        data[projectIndex].links.forEach(link => {
-            document.getElementById("project-links-list").insertAdjacentHTML('beforeend', `
+if (document.getElementById("slideshow-container")) {
+  fetch("./projectsinfo.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // SlideShow images
+      data[projectIndex].slideShowImg.forEach((image) => {
+        document.getElementById("slideshow-container").insertAdjacentHTML(
+          "beforeend",
+          `
+            <img class="my-slide-image ${
+              image === 0 ? "active" : ""
+            }" src="${image}">`
+        );
+      });
+      //
+      projectImages = document.querySelectorAll(".my-slide-image");
+      // Insert project name into destination link
+      document.getElementById("project-destination").innerText =
+        data[projectIndex].name;
+      // Insert project name
+      document.getElementById("project-name").innerText =
+        data[projectIndex].name;
+      // Insert project description
+      document.getElementById("project-description").innerText =
+        data[projectIndex].description.join("");
+      // Insert links
+      data[projectIndex].links.forEach((link) => {
+        document.getElementById("project-links-list").insertAdjacentHTML(
+          "beforeend",
+          `
                 <ul>
                     <a href="${link[1]}" target="_blank">${link[0]}</a>
-                </ul>`);
+                </ul>`
+        );
+      });
+      // Insert next/previous buttons
+      if (projectIndex > 0 && projectIndex < data.length - 1) {
+        document
+          .getElementById("project-next-prev-container")
+          .insertAdjacentHTML(
+            "afterbegin",
+            '<a onclick="NextPreviousProject(1)">Next</a>'
+          );
+        document
+          .getElementById("project-next-prev-container")
+          .insertAdjacentHTML(
+            "afterbegin",
+            '<a onclick="NextPreviousProject(-1)">Previous</a>'
+          );
+      } else if (projectIndex == 0) {
+        document
+          .getElementById("project-next-prev-container")
+          .insertAdjacentHTML(
+            "afterbegin",
+            '<a onclick="NextPreviousProject(1)">Next</a>'
+          );
+      } else if (projectIndex == data.length - 1) {
+        document
+          .getElementById("project-next-prev-container")
+          .insertAdjacentHTML(
+            "afterbegin",
+            '<a onclick="NextPreviousProject(-1)">Previous</a>'
+          );
+      }
+      showSlides();
+      // Pause the slideshow on hover
+      projectImages.forEach((e) => {
+        e.addEventListener("mouseover", () => {
+          pauseSlideshow();
         });
-        // Insert next/previous buttons
-        if(projectIndex > 0 && projectIndex < data.length - 1){
-            document.getElementById("project-next-prev-container").insertAdjacentHTML('afterbegin', '<a onclick="NextPreviousProject(1)">Next</a>');
-            document.getElementById("project-next-prev-container").insertAdjacentHTML('afterbegin', '<a onclick="NextPreviousProject(-1)">Previous</a>');
-        }
-        else if(projectIndex == 0){
-            document.getElementById("project-next-prev-container").insertAdjacentHTML('afterbegin', '<a onclick="NextPreviousProject(1)">Next</a>');
-        }
-        else if(projectIndex == data.length - 1){
-            document.getElementById("project-next-prev-container").insertAdjacentHTML('afterbegin', '<a onclick="NextPreviousProject(-1)">Previous</a>');
-        }
-        showSlides();
-        // Pause the slideshow on hover
-        projectImages.forEach(e => {
-            e.addEventListener('mouseover', () => {
-                pauseSlideshow();
-            });
-            // Resume the slideshow when the cursor leaves the slideshow area
-            e.addEventListener('mouseout', () => {
-                resumeSlideshow();
-            });
+        // Resume the slideshow when the cursor leaves the slideshow area
+        e.addEventListener("mouseout", () => {
+          resumeSlideshow();
         });
-        // Fetch Languages from specific GitHub repository
-        fetch("https://api.github.com/repos/Edga380/"+ data[projectIndex].gitHubRepName +"/languages")
+      });
+      // Fetch Languages from specific GitHub repository
+      fetch(
+        "https://api.github.com/repos/Edga380/" +
+          data[projectIndex].gitHubRepName +
+          "/languages"
+      )
         .then((gitHubResponse) => {
-            if (!gitHubResponse.ok) {
-              throw new Error(`GitHub API request failed with status: ${gitHubResponse.status}`);
-            }
-            return gitHubResponse.json();
+          if (!gitHubResponse.ok) {
+            throw new Error(
+              `GitHub API request failed with status: ${gitHubResponse.status}`
+            );
+          }
+          return gitHubResponse.json();
         })
-        .then(gitHubData => {
-            let gitHubTechTags = [];
-            if("JavaScript" in gitHubData){
-                gitHubTechTags.push(["./images/technologiesicons/jsicon.png", gitHubData.JavaScript]);
-            }
-            if("CSS" in gitHubData){
-                gitHubTechTags.push(["./images/technologiesicons/cssicon.png", gitHubData.CSS]);
-            }
-            if("HTML" in gitHubData){
-                gitHubTechTags.push(["./images/technologiesicons/htmlicon.png", gitHubData.HTML]);
-            }
-            //Calculate total JS/CSS/HTML etc...
-            const total = gitHubTechTags.reduce((sum, array) => sum + array[1], 0);
-            // Insert html with tags
-            gitHubTechTags.forEach(tag => {
-                document.getElementById("tech-tags").insertAdjacentHTML('beforeend', `
+        .then((gitHubData) => {
+          let gitHubTechTags = [];
+          if ("JavaScript" in gitHubData) {
+            gitHubTechTags.push([
+              "./images/technologiesicons/javascript_icon.png",
+              gitHubData.JavaScript,
+            ]);
+          }
+          if ("CSS" in gitHubData) {
+            gitHubTechTags.push([
+              "./images/technologiesicons/css_icon.png",
+              gitHubData.CSS,
+            ]);
+          }
+          if ("HTML" in gitHubData) {
+            gitHubTechTags.push([
+              "./images/technologiesicons/html_icon.png",
+              gitHubData.HTML,
+            ]);
+          }
+          //Calculate total JS/CSS/HTML etc...
+          const total = gitHubTechTags.reduce(
+            (sum, array) => sum + array[1],
+            0
+          );
+          // Insert html with tags
+          gitHubTechTags.forEach((tag) => {
+            document.getElementById("tech-tags").insertAdjacentHTML(
+              "beforeend",
+              `
                 <div class="tag">
                     <img src="${tag[0]}">
-                    <p>${CalculatePercentageOfTotal(tag[1], total).toFixed(1)}%</p>
-                </div>`)});
+                    <p>${CalculatePercentageOfTotal(tag[1], total).toFixed(
+                      1
+                    )}%</p>
+                </div>`
+            );
+          });
         })
-        .catch(error => {
-            console.error(error);
-            data[projectIndex].tags.forEach(tag => {
-                document.getElementById("tech-tags").insertAdjacentHTML('beforeend', `
+        .catch((error) => {
+          console.error(error);
+          data[projectIndex].tags.forEach((tag) => {
+            document.getElementById("tech-tags").insertAdjacentHTML(
+              "beforeend",
+              `
                 <div class="tag">
                     <img src="${tag[0]}">
                     <p>${tag[1]}%</p>
-                </div>`)});
-        })
+                </div>`
+            );
+          });
+        });
     });
 }
 
 // Calculates percentage of total
-function CalculatePercentageOfTotal(individualValue, total){
-    return (individualValue / total) * 100;
+function CalculatePercentageOfTotal(individualValue, total) {
+  return (individualValue / total) * 100;
 }
 
 // Project slideShow
@@ -128,42 +189,46 @@ let index = 0;
 let showSlidesInterval;
 // Previous slide
 function btnPrevious() {
-    clearTimeout(showSlidesInterval);
-    projectImages[index].classList.remove('active');
-    projectImages[index <= 0 ? projectImages.length - 1 : index - 1].classList.add('active');
-    index--;
-    if (index < 0) {
-        index = projectImages.length - 1;
-    }
-    showSlides();
-};
+  clearTimeout(showSlidesInterval);
+  projectImages[index].classList.remove("active");
+  projectImages[
+    index <= 0 ? projectImages.length - 1 : index - 1
+  ].classList.add("active");
+  index--;
+  if (index < 0) {
+    index = projectImages.length - 1;
+  }
+  showSlides();
+}
 // Next slide
 function btnNext() {
-    clearTimeout(showSlidesInterval);
-    index++;
-    if (index >= projectImages.length) {
-        index = 0;
-    }
-    showSlides();
-};
+  clearTimeout(showSlidesInterval);
+  index++;
+  if (index >= projectImages.length) {
+    index = 0;
+  }
+  showSlides();
+}
 // Slide show runs automatically
 function showSlides() {
-    projectImages.forEach((image, i) => {
-        if (i === index) {
-            image.classList.add('active');
-            projectImages[index <= 0 ? projectImages.length - 1 : index - 1].classList.remove('active');
-        }
-    });
-    showSlidesInterval = setTimeout(btnNext, 5000);
-};
+  projectImages.forEach((image, i) => {
+    if (i === index) {
+      image.classList.add("active");
+      projectImages[
+        index <= 0 ? projectImages.length - 1 : index - 1
+      ].classList.remove("active");
+    }
+  });
+  showSlidesInterval = setTimeout(btnNext, 5000);
+}
 // Pause slide show
 function pauseSlideshow() {
-    clearTimeout(showSlidesInterval);
+  clearTimeout(showSlidesInterval);
 }
 // Resume slide show
 function resumeSlideshow() {
-    showSlidesInterval = setTimeout(btnNext, 5000);
-    showSlides();
+  showSlidesInterval = setTimeout(btnNext, 5000);
+  showSlides();
 }
 
 // Toggle mobile dropbox menu
@@ -171,11 +236,11 @@ let mobNavButton = document.querySelector(".mob-nav-button");
 let dropdownMenu = document.querySelector(".mob-dropdown-menu-container");
 let lines = document.querySelectorAll(".mob-nav-line");
 
-mobNavButton.addEventListener("click", function(){
-    dropdownMenu.classList.toggle("open");
-    lines.forEach(line => {
-        line.classList.toggle("line");
-    });
+mobNavButton.addEventListener("click", function () {
+  dropdownMenu.classList.toggle("open");
+  lines.forEach((line) => {
+    line.classList.toggle("line");
+  });
 });
 
 // Toggle mobile projects dropdown menu
@@ -183,51 +248,68 @@ let mobProjectsButton = document.getElementById("mob-projects-button");
 let subDropdownMenu = document.querySelector(".mob-sub-dropdown-menu");
 let arrows = document.querySelectorAll(".mob-arrow-line");
 
-mobProjectsButton.addEventListener("click", function(){
-    arrows.forEach(arrow => {
-        arrow.classList.toggle("open");
-    });
-    subDropdownMenu.classList.toggle("open");
+mobProjectsButton.addEventListener("click", function () {
+  arrows.forEach((arrow) => {
+    arrow.classList.toggle("open");
+  });
+  subDropdownMenu.classList.toggle("open");
 });
 
 // Populate mobile dropdown menu with projects from json file
-fetch('./projectsinfo.json')
-    .then(response => response.json())
-    .then(data => {
-        data.forEach((names, i) => {
-            subDropdownMenu.insertAdjacentHTML('beforeend', `
-            <a href="./project.html" onclick="ProjectIndex(${i})">${names.name}</a>`);
-            document.querySelector(".dropdown-menu-container").insertAdjacentHTML('beforeend', `
-            <a href="./project.html" onclick="ProjectIndex(${i})">${names.name}</a>`);
-        });
-});
+fetch("./projectsinfo.json")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((names, i) => {
+      subDropdownMenu.insertAdjacentHTML(
+        "beforeend",
+        `
+            <a href="./project.html" onclick="ProjectIndex(${i})">${names.name}</a>`
+      );
+      document.querySelector(".dropdown-menu-container").insertAdjacentHTML(
+        "beforeend",
+        `
+            <a href="./project.html" onclick="ProjectIndex(${i})">${names.name}</a>`
+      );
+    });
+  });
 
 // Populate projects.html with projects
 const cardContainer = document.querySelector(".card-container");
-if(cardContainer){
-    fetch('./projectsinfo.json')
-        .then(response => response.json())
-        .then(data => {
-            // Counter for changing card size
-            let counter = 0;
-            // For loop to insert html elements
-            data.forEach((projects, i) => {
-                // Check if you are on /index.html page if yes limit projects that being shown to 4
-                if(window.location.pathname === "/index.html" && i > 3){
-                    return;
-                }
-                cardContainer.insertAdjacentHTML('beforeend', `
-                <div class="card ${(i + counter) % 2 === 0 ? 'large-project-card' : 'small-project-card'}">
+if (cardContainer) {
+  fetch("./projectsinfo.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // Counter for changing card size
+      let counter = 0;
+      // For loop to insert html elements
+      data.forEach((projects, i) => {
+        // Check if you are on /index.html page if yes limit projects that being shown to 4
+        if (window.location.pathname === "/index.html" && i > 3) {
+          return;
+        }
+        cardContainer.insertAdjacentHTML(
+          "beforeend",
+          `
+                <div class="card ${
+                  (i + counter) % 2 === 0
+                    ? "large-project-card"
+                    : "small-project-card"
+                }">
                     <div class="image-container">
-                        <img src="${projects.slideShowImg[0]}" alt="${projects.name}">
+                        <img src="${projects.slideShowImg[0]}" alt="${
+            projects.name
+          }">
                     </div>
                     <div class="card-content">
-                        <a href="./project.html" onclick="ProjectIndex(${i})">${projects.name}</a>
+                        <a href="./project.html" onclick="ProjectIndex(${i})">${
+            projects.name
+          }</a>
                         <p>${projects.description[0]}</p>
                     </div>
-                </div>`);
-                // Here we change counter so that card size changes every time loop runs
-                i % 2 === 0 ? counter += 2 : counter++;
-            });
+                </div>`
+        );
+        // Here we change counter so that card size changes every time loop runs
+        i % 2 === 0 ? (counter += 2) : counter++;
+      });
     });
 }
